f621aaa9448db21207659981bf489aa073623fa3
bianca.zabroski <bianca.zabroski@e-core.com>
2023-02-03 10:57:36 -0300

    corrected file name on html; console.log removed from start-button.js

--- src/start-button.js
+++ src/start-button.js
@@ -2,14 +2,12 @@ window.onload = function() { const startButton = document.getElementById("startB
 startButton.addEventListener("click", Start);
 
     function Start() {
-        console.log("Started");
         startButton.removeEventListener("click", Start);
         startButton.addEventListener("click", Stop);
         startButton.value = "Stop";
     }
 
     function Stop() {
-        console.log("Stopped");
         startButton.removeEventListener("click", Stop);
         startButton.addEventListener("click", Start);
         startButton.value = "Start";
