b13ada5ee49a0aa29838b3ef3c84d87e6f51dce5
bianca.zabroski <bianca.zabroski@e-core.com>
2023-02-07 15:56:47 -0300

    event date can be defined now; event finished warning added

--- src/js/countdown-main.js
+++ src/js/countdown-main.js
@@ -1,5 +1,5 @@
 const countdownTimer = () => {
-    const countDate = new Date("Jun 18, 2023 00:00:00").getTime();
+    const countDate = new Date(document.querySelector(".date").value).getTime();
     const now = new Date().getTime();
     const timeLeft = countDate - now;
 
@@ -12,5 +12,20 @@ const countdownTimer = () => {
     document.getElementById('hours').innerText = "\n" + hours + "\n" + " hours";
     document.getElementById('minutes').innerText = "\n" + minutes + "\n" + " minutes";
     document.getElementById('seconds').innerText = "\n" + seconds + "\n" + " seconds";
-}
 
+    if(now >= countDate) {
+        Swal.fire({
+            title: 'Event finished!',
+            text: 'Modal with a custom image.',
+            imageUrl: 'https://em-content.zobj.net/source/microsoft-teams/337/party-popper_1f389.png',
+            imageWidth: 160,
+            imageHeight: 160,
+            imageAlt: 'Party Confetti',
+          })
+
+          document.getElementById('days').innerText = "\n" + "D" + "\n" + " days";
+          document.getElementById('hours').innerText = "\n" + "O" + "\n" + " hours";
+          document.getElementById('minutes').innerText = "\n" + "N" + "\n" + " minutes";
+          document.getElementById('seconds').innerText = "\n" + "E" + "\n" + " seconds";
+    }
+}
