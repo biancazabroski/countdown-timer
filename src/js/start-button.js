9a1ce074dd188b0a5c3a281de7ecf81fe454a127
bianca.zabroski <bianca.zabroski@e-core.com>
2023-02-07 15:57:55 -0300

    setInterval changed to setTimeout

--- src/js/start-button.js
+++ src/js/start-button.js
@@ -1,6 +1,6 @@
 function startButton() { 
     const button = document.querySelector(".start");
     button.addEventListener("click", function() {
-        setInterval(countdownTimer, 1000)
+        setTimeout(countdownTimer, 1000)
     })
 }
