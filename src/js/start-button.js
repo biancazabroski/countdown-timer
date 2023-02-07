61b44a7b1c97d084f1d8b647d1cec1be82ec16df
bianca.zabroski <bianca.zabroski@e-core.com>
2023-02-07 10:13:59 -0300

    removed stop function

--- src/js/start-button.js
+++ src/js/start-button.js
@@ -1,17 +1,6 @@
 function startButton() { 
     const button = document.querySelector(".start");
-    button.addEventListener("click", Start);
-    
-        function Start() {
-            setInterval(countdownTimer, 1000)
-            button.removeEventListener("click", Start);
-            button.addEventListener("click", Stop);
-            button.value = "Stop";
-        }
-    
-        function Stop() {
-            button.removeEventListener("click", Stop);
-            button.addEventListener("click", Start);
-            button.value = "Start";
-        }
+    button.addEventListener("click", function() {
+        setInterval(countdownTimer, 1000)
+    })
 }
