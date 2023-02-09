a843453287f011e20e28dd38efb7fd4f27091c97
bianca.zabroski <bianca.zabroski@e-core.com>
2023-02-09 10:45:20 -0300

    start-button.js removed

--- src/js/start-button.js
+++
@@ -1,6 +0,0 @@
-function startButton() { 
-    const button = document.querySelector(".start");
-    button.addEventListener("click", function() {
-        setTimeout(countdownTimer, 1000)
-    })
-}
